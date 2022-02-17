828a0b90ca99817babf3633639fd4756fc53c930
emgrande <58751549+emgrande@users.noreply.github.com>
2022-02-17 08:00:49 -0800

    Update wavelet transform.R

--- wavelet transform.R
+++ wavelet transform.R
@@ -2,11 +2,8 @@
 #analysis shown in Grande et al., 2022. Tidal frequencies and quasiperiodic subsurface water level variations dominate redox dynamics in a salt marsh system
 
 #notice that all the data used by Grande el al., 2022 is publically available through the ESS-Drive repository (link in the paper)
-#here I have added only one file to use as an example
+#here I have added only one file to use as an example (data.csv)
 
-#call your data
-data <- read.csv("data.csv")
-head(data)
 
 
 #DateTime --> contains date and time (at 1h intervals) m/d/y h:m
@@ -19,17 +16,14 @@ wavelets <- function(df){
   #Load the required libraries
   #install.package('biwavelet')
   #install.package('lubridate')
-  
-  library(biwavelet)
-  library(lubridate)
-  
+
   data <- df
   #convert to date
-  data$DateTime <- mdy_hm(data$DateTime) #you might have to check your date time format if you get an error. ?lubridate
+  data$DateTime <- lubridate::mdy_hm(data$DateTime) #you might have to check your date time format if you get an error. ?lubridate
   
   #the data has already been cleaned by the author,
   #we can just run the continuous wavelet transform
-  cwt <- wt(data, dt = 1)
+  cwt <-biwavelet::wt(data, dt = 1)
   
   #Calculate the global wavelet transform
   global_wt <- cwt$sigma2 * (rowSums(cwt$signif)/length(data))
@@ -39,8 +33,8 @@ wavelets <- function(df){
   #because the scales in the above-calculated cwt are in date time format,
   #I prefere to recalculate the values as numeric for the significance level
   data1 <- data.frame(x = as.numeric(data$DateTime), y = data$mV.30)
-  cwt1 <- wt(data1, dt = 1)
-  sig_95 <- wt.sig(data1, sig.level = 0.95, scale = cwt1$scale, dt = 1, sigma2 = cwt1$sigma2)
+  cwt1 <- biwavelet::wt(data1, dt = 1)
+  sig_95 <- biwavelet::wt.sig(data1, sig.level = 0.95, scale = cwt1$scale, dt = 1, sigma2 = cwt1$sigma2)
   
   
   
@@ -50,13 +44,15 @@ wavelets <- function(df){
   
   layout(mat = matrix(c(1,1,1,1,2,2), nrow = 2))
   par(mar = c(7,4,2,1))
+  
+  print("Plotting wavelet transform...")
   #plot the cwt
   plot(cwt, xaxt = "n", xlab = "", ylab = "Period [hours]", lwd.sig = 0.3, col.coi = "grey40",# plot.cb = TRUE, legend.horiz = TRUE
        fill.cols = col, alpha.coi = 1)
   t <- data$DateTime
   xaxis <- seq(min(t), max(t), length.out = 12)
-  xaxis <- round_date(xaxis, unit = "day")
-  xaxis1 <- month(xaxis, label = T, abbr = T)
+  xaxis <- lubridate::round_date(xaxis, unit = "day")
+  xaxis1 <- lubridate::month(xaxis, label = T, abbr = T)
   axis(1, at = xaxis, labels = paste0(xaxis1))
   
   mtext("Wavelet Power Spectrum", side = 3, line = 0.5)
@@ -71,6 +67,11 @@ wavelets <- function(df){
   mtext("Global Wavelet", side = 3, line = 0.5)
   
 }
+
+
+#call your data
+data <- read.csv("data.csv")
+head(data)
 
 #run the function
 wavelets(df = data)
